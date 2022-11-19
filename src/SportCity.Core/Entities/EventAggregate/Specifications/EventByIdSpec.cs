eb8efba618eeafbf623343bc1f518d33e42f175f
Mykyta_Pedorenko <nikitnya1@gmail.com>
2022-11-19 16:48:39 +0200

    Fix organizer

--- src/SportCity.Core/Entities/EventAggregate/Specifications/EventByIdSpec.cs
+++ src/SportCity.Core/Entities/EventAggregate/Specifications/EventByIdSpec.cs
@@ -7,6 +7,7 @@ public class EventByIdSpec : EventBaseSpec, ISingleResultSpecification<Event>
     public EventByIdSpec(int id) : base()
     {
         Query
+            .Include(e => e.Organizer)
             .Where(e => e.Id == id);
     }
 }
